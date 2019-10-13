af8d40d77c1cf3cd87fe8ccd448fcbea208b2f62
Bettina Meier <bmeier01@web.de>
2019-10-12 23:38:37 -0400

    added disp info

--- server.R
+++ server.R
@@ -44,69 +44,83 @@ shinyServer(function(input, output){
       group_by(facility_sub_region) %>% 
       summarize(.,waste_in_tonnes = sum(tonnes_received)) %>% 
       mutate(.,percent_waste = round(waste_in_tonnes*100/sum(waste_in_tonnes),2)) %>% 
-      arrange(desc(percent_waste))  
-    })  
-      
- #     filter(origin == input$origin & dest == input$dest, month == input$month) %>%
- #     group_by(carrier) %>%
- #    summarise(n = n(), departure = mean(dep_delay),
- #               arrival=mean(arr_delay))
-
+      arrange(desc(percent_waste))
+    })
   
-#  output$map1 <- renderGvis({
-#    gvisGeoChart(state_stat, "state.name", input$selected,
-#                 options=list(region="US", displayMode="regions",
-#                              resolution="provinces",
-#                              width="auto", height="auto"))
-    # using width="auto" and height="auto" to
-    # automatically adjust the map size
-#  })
-#  output$map2 <- renderGvis({
-#    gvisGeoChart(state_stat, "state.name", input$selected,
-#                 options=list(region="US", displayMode="regions",
-#                              resolution="provinces",
-#                              width="auto", height="auto"))
-    # using width="auto" and height="auto" to
-    # automatically adjust the map size
-#  })
+  basic_waste_cat_site <- reactive({
+    WD_UK_2018 %>% filter(., basic_waste_cat == input$basic_waste_cat) %>% 
+    group_by(facility_sub_region) %>% 
+    summarize(.,waste_in_tonnes = sum(tonnes_received)) %>% 
+    mutate(.,percent_waste = round(waste_in_tonnes*100/sum(waste_in_tonnes),2)) %>% 
+    arrange(desc(percent_waste))
+  })
+  
+  basic_waste_cat_fate <- reactive({
+    WD_UK_2018 %>% filter(., basic_waste_cat == input$basic_waste_cat) %>% 
+      group_by(fate) %>% 
+      summarise(waste_in_tonnes = sum(tonnes_received)) %>% 
+      mutate(.,percent_waste = round(waste_in_tonnes*100/sum(waste_in_tonnes),2)) %>% 
+      arrange(desc(percent_waste))
+
+  })
+  
+  
+  #  output$map1 <- renderGvis({
+  #    gvisGeoChart(state_stat, "state.name", input$selected,
+  #                 options=list(region="US", displayMode="regions",
+  #                              resolution="provinces",
+  #                              width="auto", height="auto"))
+  
+  #  })
+  #  output$map2 <- renderGvis({
+  #    gvisGeoChart(state_stat, "state.name", input$selected,
+  #                 options=list(region="US", displayMode="regions",
+  #                              resolution="provinces",
+  #                              width="auto", height="auto"))
+  
+  #  })
   
   output$Pie <- renderGvis({
     waste_in_percent <- columnName()
-       #WD_UK_2018 %>%
-       #group_by() %>% # replaced by input
-       #summarize(.,waste_in_tonnes = sum(tonnes_received)) %>% 
-       #mutate(.,percent_waste = round(waste_in_tonnes*100/sum(waste_in_tonnes),2)) %>%
-       #arrange(desc(percent_waste))
-    gvisPieChart(data = waste_in_percent, labelvar = colnames(waste_in_percent[1]), numvar = "percent_waste")
+    gvisPieChart(data = waste_in_percent, labelvar = colnames(waste_in_percent[1]), numvar = "percent_waste", options = list(width=600, height=300))
   })
   
+  output$table <- DT::renderDataTable({
+    columnName()
+  })
+  
+  output$bargraph_disposal <- renderPlot({
+    basic_waste_cat_site() %>% 
+      ggplot(aes(x=facility_sub_region, y=waste_in_tonnes)) +
+      geom_bar(stat="identity", fill='goldenrod1') + 
+      theme_light() +
+      theme(axis.text.x = element_text(angle = 90, hjust=0), plot.title = element_text(hjust = 0.5)) +
+      labs(title="Waste Disposal Sites", x ="", y = "Waste in Tonnes")
+  })
+  
+  output$pie_1_disposal <- renderGvis({
+    gvisPieChart(data = basic_waste_cat_fate(), labelvar = colnames(waste_in_percent[1]), numvar = "percent_waste", options = list(width=600, height=300))
+  })
   
   output$bargraph_1 <- renderPlot({
     destin_waste_origin_basic_cat() %>% 
     ggplot(aes(x=facility_sub_region, y=waste_in_tonnes)) +
       geom_bar(stat="identity", fill='royalblue1') +
+      theme_light() +
       theme(axis.text.x = element_text(size= 12, angle = 90, hjust=0)) +
       xlab("") +
-      ylab("waste in tonnes")
+      ylab("Waste in Tonnes")
   })
   
   output$bargraph_2 <- renderPlot({
     destin_waste_origin_ewc_type() %>% 
       ggplot(aes(x=facility_sub_region, y=waste_in_tonnes)) +
       geom_bar(stat="identity", fill='darkolivegreen4') +
+      theme_light() +
       theme(axis.text.x = element_text(size= 12, angle = 90, hjust=0)) +
       xlab("") +
-      ylab("waste in tonnes")
+      ylab("Waste in Tonnes")
   })
-  
-  
 
-  output$table <- DT::renderDataTable({
-    columnName()
-    #waste_in_percent_tbl <- columnName()
-    #waste_in_percent_tbl
-  })
-  
-  
  
 })
--- ui.R
+++ ui.R
@@ -7,77 +7,85 @@ library(DT)
 library(tidyverse)
 
 ## ui.R ##
-shinyUI(dashboardPage(
+shinyUI(dashboardPage(skin = "black",
+                      
   dashboardHeader(
     title = "UK Waste Statistics 2018"
   ),
+  
   dashboardSidebar(
-    sidebarUserPanel("B Meier"), #, image = <link to Your Photo>),
+    sidebarUserPanel("Bettina Meier"), #, image = <link to Your Photo>),
     sidebarMenu(
       menuItem("Topic Background", tabName = "intro", icon = icon("info-circle")),
       menuItem("Map", tabName = "map", icon = icon("map")), 
       menuItem("Waste Data Overview", tabName = "proportions", icon = icon("chart-pie")),
-      menuItem("Detailed Analysis", tabName = "data", icon = icon("database"))
-      )#,
-    
-#    selectizeInput(inputId = "waste_category",
-#                   label = "Waste Category",
-#                   choices = c("total", "population adjusted", "basic_waste_cat", "ewc_chapter"))
+      menuItem("Waste Diposal", tabName = "fate", icon = icon("recycle")),
+      menuItem("Waste Routes - Details", tabName = "data", icon = icon("truck"))
+      )
   ),
+  
   dashboardBody(
     tabItems(
+      tabItem(tabName = "intro",
+              h4("Where does our waste go?")
+              #fluidRow(box(htmlOutput("map1")),  # gvisGeochart origin
+              #         box(htmlOutput("map2")))), # gvisGeochart dest
+      ),
     tabItem(tabName = "map",
             "to be replaced with origin and dest map"
             #fluidRow(box(htmlOutput("map1")),  # gvisGeochart origin
             #         box(htmlOutput("map2")))), # gvisGeochart dest
             ),
     tabItem(tabName = "proportions",
-            # pie-chart
             fluidPage(
-            #fluidRow(
               box(selectizeInput(inputId = "waste_category",
                                  label = "Waste Category",
                                  choices = list("Basic Waste Category" = "basic_waste_cat",
-                                                "Waste Types (European Waste Catalogue)" = "ewc_chapter"))),
-              
-            # Output: Tabset w/ plot, summary, and table ----
-            #tabsetPanel(
-            #tabPanel("Plot", htmlOutput("Pie")),
-             #tabPanel("Table", DT::dataTableOutput("table")))
-            
-              box(htmlOutput("Pie"), width = 12)#, 
-            #br(),
-            
-            #  box(DT::dataTableOutput("table"), height= 300, width = 12)
-            
+                                                "Waste Types (European Waste Catalogue)" = "ewc_chapter")), width = 12),
+              box(htmlOutput("Pie"), width = 12), 
+              br(),
+              box(DT::dataTableOutput("table"), width = 12)
             )),
+    tabItem(tabName = "fate",
+            fluidPage(
+              box(selectizeInput(inputId = "basic_waste_cat",
+                                 label= "Basic Waste Category",
+                                 choices = sort(unique(WD_UK_2018$basic_waste_cat))), width = 12),
+              box(plotOutput("bargraph_disposal"), width = 12
+              ),
+              br(),
+              box(htmlOutput("pie_1_disposal"), width = 12) 
+# 
 
+             
+
+#              box(htmlOutput("Pie_2"), width = 12), 
+#              br(),
+#              box(DT::dataTableOutput("table_2"), width = 12)
+              
+            )),
     tabItem(tabName = "data",
             fluidRow(
-            box(selectizeInput(inputId = "basic_waste_cat",
-                               label= "Basic Waste Category",
-                               choices = unique(WD_UK_2018$basic_waste_cat)),
-
-                selectizeInput(inputId = "recorded_origin_1",
+            box(
+              selectizeInput(inputId = "recorded_origin_1",
                                label = "Origin",
-                               choices = unique(WD_UK_2018$recorded_origin))),
+                               choices = sort(unique(WD_UK_2018$recorded_origin))),
+              selectizeInput(inputId = "basic_waste_cat",
+                               label= "Basic Waste Category",
+                               choices = sort(unique(WD_UK_2018$basic_waste_cat))), width = 5),
 
-#              box(DT::dataTableOutput("table"),
-#                         width = 12), 
-               column(6, plotOutput("bargraph_1"))),
-            
+               column(7, plotOutput("bargraph_1"))),
             br(),
-
             fluidRow(
-              box(selectizeInput(inputId = "ewc_chapter",
+              box(
+                selectizeInput(inputId = "recorded_origin_2",
+                                 label = "Origin",
+                                 choices = sort(unique(WD_UK_2018$recorded_origin))),
+                selectizeInput(inputId = "ewc_chapter",
                                         label= "Waste Types (European Waste Catalogue)",
-                                        choices = unique(WD_UK_2018$ewc_chapter)),
-                         
-                         selectizeInput(inputId = "recorded_origin_2",
-                                        label = "Origin",
-                                        choices = unique(WD_UK_2018$recorded_origin))),
+                                        choices = sort(unique(WD_UK_2018$ewc_chapter))), width = 5),
                      
-                     column(6, plotOutput("bargraph_2")))
+                     column(7, plotOutput("bargraph_2")))
 
             )
 
